9abadd628ceb60c77656b2bf143408f0769fe834
Aashritha <aashrithabadampudi@gmail.com>
2025-12-13 19:45:38 +0530

    Added

---
+++ Day 8/6bool.py
@@ -0,0 +1,9 @@
+# 3 boolean values using and
+print(True&True&True) #Only this becomes true
+print(True&True&False)
+print(True&False&True)
+print(True&False&False)
+print(False&True&True)
+print(False&True&False)
+print(False&False&True)
+print(False&False&False)
