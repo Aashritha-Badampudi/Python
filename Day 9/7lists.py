f3667091c1720806713d2f90591cd89c33820ea6
Aashritha <aashrithabadampudi@gmail.com>
2025-12-14 20:24:31 +0530

    Added

---
+++ Day 9/7lists.py
@@ -0,0 +1,6 @@
+#Adding lists to the elements
+num=[10,20,30,40,50]
+num.append(60) #Add at the end
+num.insert(3,70) #At particular position
+num.extend([80,90]) #Extending multiple elements at the end
+print(num)
