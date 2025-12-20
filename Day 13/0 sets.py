2202150be6f9c91eee92d9bdbc81b3dc3f0c9456
Aashritha <aashrithabadampudi@gmail.com>
2025-12-20 13:36:31 +0530

    Added

---
+++ Day 13/0 sets.py
@@ -0,0 +1,14 @@
+# 🔵 What is a Set?
+# A set is a collection of unique elements.
+
+# Key points:
+# Unordered
+# No duplicate values
+# Mutable (you can add/remove elements)
+# Written using {}
+
+# 🔵 Why Do We Need Sets?
+# Sets are used when:
+# You want unique values only
+# You want to remove duplicates
+# You want fast membership checking 
