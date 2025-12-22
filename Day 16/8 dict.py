c3c100577c847ad003368a65ac4370c27dbffed9
Aashritha <aashrithabadampudi@gmail.com>
2025-12-22 18:56:45 +0530

    Added

---
+++ Day 16/8 dict.py
@@ -0,0 +1,6 @@
+#Checking membership
+student = {"Name": "Aashritha", "Age" : 19, "Collage" : "SMEC"}
+print("Name" in student)
+print("Marks" in student)
+print("Age" not in student)
+print("Marks" not in student)
