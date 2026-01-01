a847da03a009e2a348766cebc9d0f14f7168ae80
Aashritha <aashrithabadampudi@gmail.com>
2026-01-01 19:19:42 +0530

    Added

---
+++ Day 20/0 while.py
@@ -0,0 +1,21 @@
+"""
+A while loop is used when you want to repeat something again and again
+as long as a condition is True.
+
+>> Simple meaning:
+“Keep doing this WHILE the condition is true.”
+
+syntax:
+while condition:
+     statement
+
+Condition must be True or False
+Code inside must be indented
+
+while repeats code
+Condition must change
+Stops when condition becomes False
+Be careful of infinite loops
+break stops loop
+continue skips iteration
+"""
