f794671239d854099c769d8abb79102de9578820
Aashritha <aashrithabadampudi@gmail.com>
2026-01-07 20:01:32 +0530

    Added

---
+++ Day 24/1 for.py
@@ -0,0 +1,38 @@
+"""
+1. Introduction to for Loop
+-- ------------ -- --- ----
+
+* What is a for loop?
+-> A for loop is used to repeat a block of code
+once for each item in a sequence.
+
+* Why do we use a for loop?
+-> Without loops:
+    You must write the same code again and again ❌
+    Code becomes long and messy ❌
+
+-> With for loop:
+    Code becomes short ✅
+    Easy to read ✅
+    Easy to change ✅
+
+* Difference between for loop and while loop.
+___________________________________________________________________________________
+| Aspect     | `for` loop                     | `while` loop                      |
+|------------|--------------------------------|-----------------------------------|
+| Basis      | Works on sequences             | Works on conditions               |
+| Repetition | Fixed number of times          | Unknown number of times           |
+| Control    | Automatic iteration            | Manual control required           |
+| Usage      | When data is already available | When condition decides repetition |
+|____________|________________________________|___________________________________|
+
+* When to use for vs while.
+✅ Use for loop when:
+    You know how many times the loop should run
+    You are working with a sequence (string, list, range, etc.)
+    You want cleaner and simpler code
+✅ Use while loop when:
+    You don’t know how many times the loop should run
+    The loop depends on a condition
+    User input controls repetition
+"""
