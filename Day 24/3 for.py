5a2b5e42c34785c39cf9cb9154d5543e545cb9e0
Aashritha <aashrithabadampudi@gmail.com>
2026-01-08 19:01:36 +0530

    Added

---
+++ Day 24/3 for.py
@@ -0,0 +1,22 @@
+"""
+* What is range()?
+range() is a built-in Python function used to generate a sequence of numbers.
+
+Important points:
+range() generates numbers one by one
+It is mostly used with for loops
+It does not print anything by itself
+It helps avoid manual counting
+
+In simple words:
+range() provides numbers for the for loop to iterate over.
+
+"range(stop)"
+Meaning:
+Starts from 0
+Ends at stop -1
+Step size is 1 by default.
+
+"""
+for i in range(5):
+    print(i)
