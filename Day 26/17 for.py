9b871ca764e034c018d43ddb0b71615b5562b068
Aashritha <aashrithabadampudi@gmail.com>
2026-01-10 20:00:33 +0530

    Added

---
+++ Day 26/17 for.py
@@ -0,0 +1,13 @@
+#Break continue pass in for
+for i in range(1,6):
+    # if i==3:
+    #     break
+    # print(i)
+
+    # if i==3:
+    #     continue
+    # print(i)
+
+    if i==2:
+        pass
+    print(i)
