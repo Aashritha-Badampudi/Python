dbc245f1979e70868cc47122107c8687f20d8c47
Aashritha <aashrithabadampudi@gmail.com>
2026-01-10 20:20:24 +0530

    Added

---
+++ Day 26/20 for.py
@@ -0,0 +1,28 @@
+"""Nested for loops
+A nested loop means:
+    A loop inside another loop
+
+In Python:
+The outer loop runs first
+For each iteration of the outer loop, the inner loop runs completely
+
+In simple words:
+The inner loop finishes all its work every time the outer loop runs once.
+
+Outer Loop vs Inner Loop
+🔹 Outer Loop
+The main loop
+Controls how many times the inner loop will repeat
+Runs fewer times compared to inner loop
+
+🔹 Inner Loop
+The loop inside the outer loop
+Runs completely for each outer loop iteration
+Executes more times overall
+
+Outer loop → 1 iteration
+   Inner loop → full execution
+
+Outer loop → next iteration
+   Inner loop → full execution again
+"""
