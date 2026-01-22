8e2379d948b8956c359a04eb4cf081a1c7255d76
Aashritha <aashrithabadampudi@gmail.com>
2026-01-22 15:18:16 +0530

    Added

---
+++ Day 33/47 for.py
@@ -0,0 +1,9 @@
+#Reverse hollow right angle
+n=6
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        if j==1 or j==i or i==n:
+            print("* ",end="")
+        else:
+            print("  ",end="")
+    print()
