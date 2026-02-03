b5452222e63f789633f4e25d923f46b845344580
Aashritha <aashrithabadampudi@gmail.com>
2026-02-03 15:06:58 +0530

    Added

--- Day 44/func3.py
+++ Day 44/func3.py
@@ -1,8 +1,8 @@
 #Arguements and parameters
-def my_fun(fname):
+def my_fun(fname):  #This  is a parameter
     print("My friend name is:",fname)
 
-my_fun("Navya")
+my_fun("Navya") #These are the arguements
 my_fun("Shreeya")
 my_fun("Keerthi")
 my_fun("Manasa")
