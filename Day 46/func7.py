8065a6216500100fd5630e75a9b21f9f360a33e0
Aashritha <aashrithabadampudi@gmail.com>
2026-02-05 23:18:14 +0530

    Added

---
+++ Day 46/func7.py
@@ -0,0 +1,9 @@
+def my_fun():
+    return ["Orange","Apple","Mango","Banana","Strawberry"]
+
+fruits=my_fun()
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+print(fruits[4])
