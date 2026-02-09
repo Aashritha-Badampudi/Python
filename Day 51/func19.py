1c5a2b7941ba641b35883f9934616e8d95581454
Aashritha <aashrithabadampudi@gmail.com>
2026-02-09 19:40:02 +0530

    Added

---
+++ Day 51/func19.py
@@ -0,0 +1,12 @@
+#Fibonacci
+def fibonacci(num):
+    a,b=0,1
+    for i in range(num):
+        print(a,end=" ")
+        a,b=b,a+b
+
+fibonacci(9) 
+print("\t")
+fibonacci(10)
+print("\t")
+fibonacci(20)
