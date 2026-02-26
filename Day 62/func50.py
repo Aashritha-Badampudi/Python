88fea4d7a3c9238fb7ec903c05b87fc726c85a69
Aashritha <aashrithabadampudi@gmail.com>
2026-02-26 23:30:52 +0530

    Added

---
+++ Day 62/func50.py
@@ -0,0 +1,39 @@
+# Python Decorators
+# 1️⃣ What is a Decorator?
+# ✅ Simple Definition
+
+# A decorator is a function that adds extra behavior to another function
+# without changing the original function code.
+
+# 🎂 Real-Life Example
+
+# Think of a cake 🎂
+
+# Cake = original function
+
+# Chocolate topping = decorator
+
+# Decorator adds extra features without changing the cake.
+
+# 🧠 Simple Idea
+
+# Instead of editing the original function,
+# we “wrap” it inside another function.
+
+# 2️⃣ Why Use Decorators?
+
+# Decorators are used when:
+
+# ✔ You want to add extra functionality
+# ✔ You don’t want to rewrite the original function
+# ✔ You want to reuse code
+
+# 🔥 Real Example Uses
+
+# Logging (printing before and after function runs)
+
+# Checking login authentication
+
+# Measuring execution time
+
+# Validation
